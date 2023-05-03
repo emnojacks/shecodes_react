a292dd8ee9eccb32c86089a28664a64705f44ae2
Em J <79600694+emnojacks@users.noreply.github.com>
2023-05-02 20:49:23 -0600

    created footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import "./styles.css";
 import WeatherSearch from "./WeatherSearch";
-
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather Search</h1>
-       <WeatherSearch />
+        <WeatherSearch />
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      <a
+        href="https://github.com/emnojacks/shecodes_react"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Github Repo
+      </a>
+    </p>
+  );
+}
