83ca7e2d5f14a99111bc08cb57d018010f87905a
Em J <79600694+emnojacks@users.noreply.github.com>
2023-05-07 20:52:19 -0600

    created formatted date

--- src/DateFormatted.js
+++ src/DateFormatted.js
@@ -1,11 +1,39 @@
 import React from "react";
 
-
 export default function DateFormatted(props) {
+  console.log(props.date);
+  //var decs
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let mos = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const day = days[props.date.getDay()];
+  const month = mos[props.date.getMonth()];
+  const date = props.date.getDate();
+  const hrs = props.date.getHours();
+  const mins = props.date.getMinutes();
 
-    return (
-<h1>date</h1>
-)
-    
-    
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  if (hrs < 10) {
+    hrs = `0${hrs}`;
+  }
+
+  return (
+    <p>
+      {day} {month} {date} {hrs}:{mins}
+    </p>
+  );
 }
