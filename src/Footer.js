14283deafcaf687a1a4b90555cdaa5615d7a28a4
Em J <79600694+emnojacks@users.noreply.github.com>
2023-05-15 16:01:13 -0600

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,26 @@ export default function Footer() {
   return (
     <div className="footer-div">
       <p>
+        {" "}
+        This project was by Em J coded for&nbsp;
+        <a href="http://shecodes.io/" rel="noopener noreferrer" target="_blank">
+          SheCodes
+        </a>
+        &nbsp; open-sourced&nbsp;
         <a
           href="https://github.com/emnojacks/shecodes_react"
           rel="noopener noreferrer"
           target="_blank"
         >
-          Github Repo
+          on GitHub
+        </a>
+        &nbsp; and hosted&nbsp;
+        <a
+          href="https://646292fa01e7820008707bf7--voluble-torrone-020515.netlify.app/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          on Netlify
         </a>
       </p>
     </div>
